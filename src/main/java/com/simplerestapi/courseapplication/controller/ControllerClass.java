ca26fa43c0187f3ffab2f5d0e014d8fb0ad695fc
PadavalaPavanKumar <pavankumar.padavala@kanerika.com>
2022-12-06 17:28:19 +0530

    my first commit

---
+++ src/main/java/com/simplerestapi/courseapplication/controller/ControllerClass.java
@@ -0,0 +1,13 @@
+package com.simplerestapi.courseapplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerClass {
+	@GetMapping("/home")
+	public String home() {
+		return "this is my home page";
+
+	}
+}
